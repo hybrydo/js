43606937215e79efdf9850a3b244730616288d29
hybrydo <hybrydo@gmail.com>
2018-07-05 15:05:31 +0200

    añadiendo mas contenido

--- jquery/06-interface/js/06-interface.js
+++ jquery/06-interface/js/06-interface.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
+
+    //mover elemento por la página
     $('.elemento').draggable();
+
+    //redimensionar
+    $('.elemento').resizable();
+
+    //seleccionar y ordenar elementos
+   // $(".lista-seleccionable").selectable();
+    $(".lista-seleccionable").sortable();
+
+    //drop
+
+    $("€elemento-movido").draggable();
+    $("#area").droppable({
+        drop: function(){
+            console.log("has soltado algo dentro de el area");
+        }
+    });
+    
 });
